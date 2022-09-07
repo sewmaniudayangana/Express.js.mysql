c11bceb92decad6a7a9ee7ad8045c4667ddff557
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-09-07 16:57:21 +0530

    update

--- routes/item.js
+++ routes/item.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const connection  =mysql.createConnection(databa.database)
 
 
-//item table
 connection.connect(function (err) {
     if(err) {
         console.log(err);
@@ -17,3 +16,85 @@ connection.connect(function (err) {
         })
     }
 }) 
+
+
+router.get('/', (req, res) => {
+    var getAllitemQuery = "SELECT * FROM item";
+    connection.query (getAllitemQuery,(err, rows) => {
+         if(err) console.log(err);
+         res.send(rows);
+    })
+})
+
+ 
+router.post('/',(req, res) => {
+    const itemcode = req.body.itemcode;
+    const name = req.body.name;
+    const qtyonHand = req.body.qtyonHand;
+    const unitprice = req.body.unitprice;
+
+    var saveitemQuery = "INSERT INTO item(itemcode,name,qtyonHand,unitprice) VALUES(?,?,?,?)";
+
+    connection.query(saveitemQuery,[itemcode,name,qtyonHand,unitprice], (err) => {
+        if (err) {
+            res.send({"message" : "same key"})
+
+        } else {
+            res.send({"message" : "item details is ok"})
+        }
+    }) 
+})
+
+
+router.put('/', (req, res) => {
+    const itemcode = req.body.itemcode;
+    const name = req.body.name;
+    const qtyonHand = req.body.qtyonHand;
+    const unitprice = req.body.unitprice;
+
+    var updateitemQuery = "UPDATE item SET name=?,address=?,salary=? WHERE itemcode=?";
+    connection.query(updateitemQuery, [name, qtyonHand, unitprice, itemcode], (err, rows) => {
+        if (err) {
+            res.send({"message" : "item update"})
+
+        } else {
+            res.send({"message" : "item is error. try again"})
+        }
+    })
+})
+
+
+
+router.get('/:itemcode', (req,res) => {
+    const itemcode = req.params.itemcode;
+
+    var searchitemQuery = "SELECT * FROM item WHERE itemcode=?";
+
+    connection.query(searchitemQuery, [itemcode], (err, row) => {
+        if(err) console.log(err);
+        res.send(rows);
+    })
+})
+
+
+
+router.delete('/:itemcode', (req,res) => {
+    const id = req.params.id;
+    var deleteQuery = "DELETE FROM item WHERE id=?";
+
+    connection.query(deleteQuery, [id], (err, rows) => {
+        if(err) console.log(err);
+
+        if(rows.affectedRows > 0) {
+            res.send({"message" : " data deleted"})
+        } else {
+            res.send({"message" : "data was not found. try it"})
+        }
+    })
+})
+
+
+
+
+
+module.exports = router;
